4497443add0bc3c468ecc8d53b2f379482f11552
Олег Максименко <oleg.maksimenko@me.com>
2020-08-16 22:28:20 +0300

    set up minimum deployment version

---
+++ coreML-experiment/coreML-experiment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coreML-experiment
+//
+//  Created by Олег Максименко on 16.08.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
